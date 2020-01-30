4e125f5e8b3f48d0e1538a6e1f7a3d798c7fdd46
Lain Musgrove <lain.proliant@gmail.com>
2020-01-30 12:22:56 -0800

    Bump version in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="panifex",
-    version="0.2",
+    version="0.3",
     description="A make-like DI-based Python build system.",
     long_description=long_description,
     long_description_content_type="text/markdown",
